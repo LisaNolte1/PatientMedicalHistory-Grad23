4ff27b1637957a5721c3ab7e4f77cf2ab0e01175
Mikhail Govender <mikhailg@bbd.co.za>
2023-02-19 12:05:50 +0200

    Adding stored procs and view source code

---
+++ MedicalHistoryStoredProc.sql
@@ -0,0 +1,51 @@
+USE MedicalHistory;
+GO
+
+
+--INSERT NEW PRESCRIPTION--
+CREATE PROCEDURE [dbo].[uspInsertNewPrescription]
+@medicationId uniqueidentifier,
+@patientId uniqueidentifier,
+@professionalId uniqueidentifier,
+@startdate date,
+@endate date,
+@cancelledDate datetime,
+@doseId uniqueidentifier
+AS
+INSERT INTO [dbo].[Prescription](medicationId, patientId, professionalId, startdate, endate, cancelledDate, doseId)
+VALUES(@medicationId, @patientId, @professionalId, @startdate, @endate, @cancelledDate, @doseId);
+GO
+
+--SELECT PATIENT DETAILS--
+CREATE PROCEDURE [dbo].[uspGetPatientDetailsByID]
+@patientId uniqueidentifier
+AS
+BEGIN
+SELECT p.id, pr.name, pr.surname, c.phone, c.email
+FROM Patient p
+INNER JOIN Person pr ON pr.id = p.id
+INNER JOIN Contact c ON c.id = p.contactId
+WHERE p.id = @patientId;
+END
+GO
+
+EXEC uspGetPatientDetailsByID 'B551F2C8-8380-491B-A51F-436E51CDD08F';
+GO
+
+--FULL PATIENT HISTORY VIEW--
+--Patient detials [id,name, surname,idnum,contact,prescriptions,procedure,doctor]--
+CREATE VIEW [Full_Patient_History]
+AS
+SELECT p.id, p.name, p.surname, p.idNumber, c.phone, c.email, m.name AS Medication,
+	   d.description AS Doseage, pr.startdate, pr.endate, pr.cancelledDate, prct.description AS PateintProcedure, prc.date AS ProcedureDate
+FROM Person p
+INNER JOIN Contact c ON c.id = p.id
+INNER JOIN Prescription pr ON pr.patientId = p.id
+INNER JOIN Medication m ON pr.medicationId = m.id
+INNER JOIN Dose d ON d.id =pr.doseId
+INNER JOIN PatientProcedure prc ON prc.patientId = p.id
+INNER JOIN ProcedureType prct ON prct.id = prc.typeId;
+GO
+
+SELECT * FROM [Full_Patient_History];
+GO
